cbfad00cd0fcbf0f7a5f16dbe1dabce7b5580456
Saswat Padhi <saswat.sourav@gmail.com>
2020-08-30 19:29:30 -0400

    fixed CI issues

--- unms/docker-compose.up.pre_hook.sh
+++ unms/docker-compose.up.pre_hook.sh
@@ -8,4 +8,9 @@ SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 # This should be done within the unms Dockerfile!
 # It initializes a bunch of directories, but not cache.
 mkdir -p "$SELF_DIR/data/unms/cache"
-chown 1000 "$SELF_DIR/data/unms/cache"
+
+USER_ID=$(id -u)
+
+cat << EOF > "$SELF_DIR/.env"
+USER_ID=$USER_ID
+EOF
