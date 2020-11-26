101efddd1add88c5ce6f193a3a7dac954eb231ee
Saswat Padhi <saswat.sourav@gmail.com>
2020-11-26 08:37:02 -0500

    fixed issues with docker bridge network

--- qbittorrent/docker-compose.up.pre_hook.sh
+++ qbittorrent/docker-compose.up.pre_hook.sh
@@ -5,7 +5,8 @@ set -Eumo pipefail
 SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 mkdir -p $SELF_DIR/data/caddy/config \
-         $SELF_DIR/data/caddy/data
+         $SELF_DIR/data/caddy/data \
+         $SELF_DIR/data/qbittorrent/config
 
 cat << EOF > "$SELF_DIR/.env"
 TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
