a2133365a2dcb648b7e8ad671775004301afc845
Saswat Padhi <saswat.sourav@gmail.com>
2020-11-26 23:31:38 -0500

    qbittorrent data dir for logs

--- qbittorrent/docker-compose.up.pre_hook.sh
+++ qbittorrent/docker-compose.up.pre_hook.sh
@@ -6,7 +6,8 @@ SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 mkdir -p $SELF_DIR/data/caddy/config \
          $SELF_DIR/data/caddy/data \
-         $SELF_DIR/data/qbittorrent/config
+         $SELF_DIR/data/qbittorrent/config \
+         $SELF_DIR/data/qbittorrent/data
 
 cat << EOF > "$SELF_DIR/.env"
 TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
