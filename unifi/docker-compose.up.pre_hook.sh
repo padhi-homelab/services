c05b9f987c0ce626eaa2576b1eb1c4b2a8be96db
Saswat Padhi <saswatpadhi@protonmail.com>
2021-10-20 16:05:31 +0000

    fix unifi image and paths

--- unifi/docker-compose.up.pre_hook.sh
+++ unifi/docker-compose.up.pre_hook.sh
@@ -4,6 +4,11 @@ set -Eumo pipefail
 
 SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
+mkdir -p $SELF_DIR/data/mongo/data/db \
+         $SELF_DIR/data/unifi/usr/lib/unifi/cert \
+         $SELF_DIR/data/unifi/usr/lib/unifi/data \
+         $SELF_DIR/data/unifi/usr/lib/unifi/logs
+
 cat << EOF > "$SELF_DIR/.env"
 GROUP_ID=$(id -g)
 TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
