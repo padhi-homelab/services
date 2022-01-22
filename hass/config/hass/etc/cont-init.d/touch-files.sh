328f0ea0d3c0b9e16c26a5d8be4c26e310b970e7
Saswat Padhi <saswatpadhi@protonmail.com>
2022-01-22 08:53:20 +0000

    fix hass

--- hass/config/hass/etc/cont-init.d/touch-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo [] > /config/automations.yaml
-touch /config/groups.yaml \
-      /config/scenes.yaml \
-      /config/secrets.yaml \
-      /config/scripts.yaml
--- hass/docker-compose.up.pre_hook.sh
+++ hass/docker-compose.up.pre_hook.sh
@@ -9,4 +9,5 @@ mkdir -p $SELF_DIR/data/hass/config
 cat << EOF > "$SELF_DIR/.env"
 TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
 USER_ID=$(id -u)
+GROUP_ID=$(id -g)
 EOF
