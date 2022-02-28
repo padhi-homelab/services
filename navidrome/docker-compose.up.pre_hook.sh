b48be25a3ee0880ae0d4ea12dd0d659f4a84d3b1
Saswat Padhi <saswatpadhi@protonmail.com>
2022-02-28 23:09:27 +0000

    added navidrome

---
+++ navidrome/docker-compose.up.pre_hook.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -Eumo pipefail
+
+SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+mkdir -p $SELF_DIR/data/navidrome/data
+
+cat << EOF > "$SELF_DIR/.env"
+GROUP_ID=$(id -g)
+TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
+USER_ID=$(id -u)
+EOF
