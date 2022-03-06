575a2866d8f3614a7a1678e1e9170d1c45c57442
Saswat Padhi <saswatpadhi@protonmail.com>
2022-03-06 15:16:24 +0000

    minor

--- i2p/docker-compose.up.pre_hook.sh
+++ i2p/docker-compose.up.pre_hook.sh
@@ -4,7 +4,9 @@ set -Eumo pipefail
 
 SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
-mkdir -p $SELF_DIR/data/i2p/i2p/.i2p \
+mkdir -p $SELF_DIR/data/caddy/config \
+         $SELF_DIR/data/caddy/data \
+         $SELF_DIR/data/i2p/i2p/.i2p \
          $SELF_DIR/data/i2p/i2psnark
 
 cat << EOF > "$SELF_DIR/.env"
