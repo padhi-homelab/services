d5ce1c60be9f26b29beebe9ccd01a66992c5a8c6
Saswat Padhi <saswatpadhi@protonmail.com>
2022-07-31 18:03:55 +0000

    and some more Nextcloud tweaks

--- nextcloud/docker-compose.up.pre_hook.sh
+++ nextcloud/docker-compose.up.pre_hook.sh
@@ -7,6 +7,8 @@ SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 mkdir -p $SELF_DIR/data/caddy/config \
          $SELF_DIR/data/caddy/data \
          $SELF_DIR/data/mariadb/var/lib/mysql \
+         $SELF_DIR/data/redis/redis \
+         $SELF_DIR/data/nextcloud/data \
          $SELF_DIR/data/nextcloud/var/www/html \
          $SELF_DIR/data/nextcloud/usr/local/etc/php/conf.d
          
