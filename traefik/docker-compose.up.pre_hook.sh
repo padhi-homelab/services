ab093161a37c30203bd2e53b320e1c5a0f03794a
Saswat Padhi <saswatpadhi@protonmail.com>
2022-08-09 04:58:42 +0000

    traefik proxy network

--- traefik/docker-compose.up.pre_hook.sh
+++ traefik/docker-compose.up.pre_hook.sh
@@ -7,3 +7,11 @@ SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 mkdir -p $SELF_DIR/data/traefik/cert \
          $SELF_DIR/data/traefik/etc/traefik \
          $SELF_DIR/data/traefik/var/log/traefik
+
+docker network inspect \
+               proxy \
+               &> /dev/null \
+|| \
+docker network create \
+               --attachable \
+               proxy
