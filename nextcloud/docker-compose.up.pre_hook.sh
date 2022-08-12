a66997e4e529b4e416ad32c4828f75479e796e26
Saswat Padhi <saswatpadhi@protonmail.com>
2022-08-12 21:48:23 +0000

    traefik-ify nextcloud towards resolving #2

--- nextcloud/docker-compose.up.pre_hook.sh
+++ nextcloud/docker-compose.up.pre_hook.sh
@@ -4,12 +4,15 @@ set -Eumo pipefail
 
 SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
-mkdir -p $SELF_DIR/data/caddy/config \
-         $SELF_DIR/data/caddy/data \
-         $SELF_DIR/data/mariadb/var/lib/mysql \
+mkdir -p $SELF_DIR/data/mariadb/var/lib/mysql \
          $SELF_DIR/data/redis/redis \
          $SELF_DIR/data/nextcloud/data \
          $SELF_DIR/data/nextcloud/var/www/html \
          $SELF_DIR/data/nextcloud/usr/local/etc/php/conf.d
-         
+
+# Workaround: https://github.com/nextcloud/docker/issues/763#issuecomment-1007447212
 touch $SELF_DIR/data/nextcloud/usr/local/etc/php/conf.d/redis-session.ini
+
+# Workaround: https://github.com/nextcloud/docker/issues/1494#issuecomment-1213540687 
+mkdir -p $SELF_DIR/data/nextcloud/etc/apache2/conf-enabled
+touch $SELF_DIR/data/nextcloud/etc/apache2/conf-enabled/remoteip.conf
