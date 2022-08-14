584663d3aca23f154f92156ade5c3da13b813f5e
Saswat Padhi <saswatpadhi@protonmail.com>
2022-08-14 21:34:54 +0000

    logging

--- traefik/docker-compose.up.pre_hook.sh
+++ traefik/docker-compose.up.pre_hook.sh
@@ -5,5 +5,4 @@ set -Eumo pipefail
 SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 mkdir -p "$SELF_DIR/data/traefik/cert" \
-         "$SELF_DIR/data/traefik/etc/traefik" \
-         "$SELF_DIR/data/traefik/var/log/traefik"
+         "$SELF_DIR/data/traefik/etc/traefik"
