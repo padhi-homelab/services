8834540287645504a433595487e05a20f3835686
Saswat Padhi <saswatpadhi@protonmail.com>
2022-08-18 21:14:16 +0000

    fix mariadb healthcheck

--- mariadb/config/mariadb/healthcheck.sh
+++ mariadb/config/mariadb/healthcheck.sh
@@ -4,6 +4,7 @@ set -Eumo pipefail
 
 find /profiles -maxdepth 1 -type f -name '*' -print0 | \
 while IFS= read -r -d '' profile ; do
+  [ ! -f "$profile.custom" ] || continue
   source "$profile"
   mysql "$MYSQL_DATABASE" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e 'SELECT 1;' || exit 1
 done
