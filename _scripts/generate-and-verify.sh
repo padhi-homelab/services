7f5cde44f1ee5cab5021b3edd6adcb33f7fab35f
Saswat Padhi <saswatpadhi@protonmail.com>
2022-10-26 01:53:21 +0000

    traefik anti-ddos and minor tweaks

--- _scripts/generate-and-verify.sh
+++ _scripts/generate-and-verify.sh
@@ -9,19 +9,20 @@ ENV_VARS="$(cut -d= -f1 .env | awk '{print "$" $0}')"
 
 echo -n "[~] Generating '$TARGET_FILE': "
 
-if [ -e "$TARGET_FILE" ] && [ ! -f "$TARGET_FILE" ]; then
+if [ -e "$TARGET_FILE" ] && [ ! -f "$TARGET_FILE" ] ; then
   echo "CANNOT OVERWRITE!"
   exit 1
 fi
 
-env -i $(paste .env) \
+env -i - $(paste .env) \
     envsubst "$ENV_VARS"\
         < "$SOURCE_FILE" \
         > "$TARGET_FILE"
+[ $? -eq 0 ] || exit 1
 
 SRC_VARS=$(envsubst -v "$(cat "$SOURCE_FILE")" | paste -sd '|')
 
-if grep -qE "$SRC_VARS" "$TARGET_FILE"; then
+if grep -qE "$SRC_VARS" "$TARGET_FILE" ; then
   LINE="$(grep -E "$SRC_VARS" "$TARGET_FILE")"
   LEFTOVER_VAR="$(envsubst -v "$LINE" | paste -sd '|')"
   echo "\`$LEFTOVER_VAR\` NOT DEFINED!"
