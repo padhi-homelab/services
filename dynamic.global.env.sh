d1d016dfa2b4fa69dd840bfaafa35338b0af3be7
Saswat Padhi <saswatpadhi@protonmail.com>
2022-11-17 03:51:33 +0000

    remove unnecessary quotes

--- dynamic.global.env.sh
+++ dynamic.global.env.sh
@@ -3,8 +3,8 @@
 set -Eumo pipefail
 
 cat << EOF
-GROUP_ID='$(id -g)'
-HOST_NAME='$(hostname)'
-USER_ID='$(id -u)'
-TZ='$(timedatectl | awk '/Time zone:/ {print $3}')'
+GROUP_ID=$(id -g)
+HOST_NAME=$(hostname)
+USER_ID=$(id -u)
+TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
 EOF
--- traefik/dynamic.env.sh
+++ traefik/dynamic.env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$SERVER_LETS_ENCRYPT_ACME_CA_SERVER_USE_STAGING" = "NO" ]; then
-  echo 'SERVER_LETS_ENCRYPT_ACME_CA_SERVER="https://acme-v02.api.letsencrypt.org/directory"'
+  echo 'SERVER_LETS_ENCRYPT_ACME_CA_SERVER=https://acme-v02.api.letsencrypt.org/directory'
 else
-  echo 'SERVER_LETS_ENCRYPT_ACME_CA_SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"'
+  echo 'SERVER_LETS_ENCRYPT_ACME_CA_SERVER=https://acme-staging-v02.api.letsencrypt.org/directory'
 fi
