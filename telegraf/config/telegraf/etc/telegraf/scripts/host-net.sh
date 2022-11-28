70a6dd5a71cf1981064b41b50d33adc80f765fcd
Saswat Padhi <saswatpadhi@protonmail.com>
2022-11-28 02:06:35 +0000

    [telegraf] host_net use standard field names

--- telegraf/config/telegraf/etc/telegraf/scripts/host-net.sh
+++ telegraf/config/telegraf/etc/telegraf/scripts/host-net.sh
@@ -17,20 +17,20 @@ for PATTERN in $@ ; do
 done
 
 for IFACE in $(ls "$HOST_SYS/class/net" | grep -E "$GREP_PATTERN") ; do
-  while [ -z ${BYTES_SENT} ] || [ -z ${BYTES_RECV} ] ||
-        [ -z ${PACKETS_SENT} ] || [ -z ${PACKETS_RECV} ] ||
-        [ -z ${ERR_IN} ] || [ -z ${ERR_OUT} ] ||
-        [ -z ${DROP_IN} ] || [ -z ${DROP_OUT} ]
+  while [ -z ${TX_BYTES} ] || [ -z ${RX_BYTES} ] ||
+        [ -z ${TX_PACKETS} ] || [ -z ${RX_PACKETS} ] ||
+        [ -z ${TX_ERRORS} ] || [ -z ${RX_ERRORS} ] ||
+        [ -z ${TX_DROPPED} ] || [ -z ${RX_DROPPED} ]
   do
-    BYTES_SENT=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_bytes)
-    BYTES_RECV=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_bytes)
-    PACKETS_SENT=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_packets)
-    PACKETS_RECV=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_packets)
-    ERR_IN=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_errors)
-    ERR_OUT=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_errors)
-    DROP_IN=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_dropped)
-    DROP_OUT=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_dropped)
+    TX_BYTES=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_bytes)
+    RX_BYTES=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_bytes)
+    TX_PACKETS=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_packets)
+    RX_PACKETS=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_packets)
+    TX_ERRORS=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_errors)
+    RX_ERRORS=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_errors)
+    TX_DROPPED=$(cat $HOST_SYS/class/net/$IFACE/statistics/tx_dropped)
+    RX_DROPPED=$(cat $HOST_SYS/class/net/$IFACE/statistics/rx_dropped)
   done
 
-  echo "host_net,interface=$IFACE bytes_sent=${BYTES_SENT}u,bytes_recv=${BYTES_RECV}u,packets_sent=${PACKETS_SENT}u,packets_recv=${PACKETS_RECV}u,err_in=${ERR_IN}u,err_out=${ERR_OUT}u,drop_in=${DROP_IN}u,drop_out=${DROP_OUT}u"
+  echo "host_net,interface=$IFACE tx_bytes=${TX_BYTES}u,rx_bytes=${RX_BYTES}u,tx_packets=${TX_PACKETS}u,rx_packets=${RX_PACKETS}u,tx_errors=${TX_ERRORS}u,rx_errors=${RX_ERRORS}u,tx_dropped=${TX_DROPPED}u,rx_dropped=${RX_DROPPED}u"
 done
