1d59f08a1048a68f50720c4e877b8c9d1662c9a9
Saswat Padhi <saswatpadhi@protonmail.com>
2024-12-09 22:57:18 -0800

    hostname or hostnamectl

--- dynamic.global.env.sh
+++ dynamic.global.env.sh
@@ -5,7 +5,7 @@ set -Eumo pipefail
 cat << EOF
 ARCHITECTURE=$(uname -m)
 GROUP_ID=$(id -g)
-HOST_NAME=$(hostname)
+HOST_NAME=$(hostname 2>/dev/null || hostnamectl hostname)
 USER_ID=$(id -u)
 TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
 EOF
