a7b42146ca9a7cdd9d5bbd51ce75d8015c76544e
Saswat Padhi <saswatpadhi@protonmail.com>
2024-12-13 11:17:58 -0800

    minor

--- dynamic.global.env.sh
+++ dynamic.global.env.sh
@@ -3,9 +3,9 @@
 set -Eumo pipefail
 
 cat << EOF
-ARCHITECTURE=$(uname -m)
-GROUP_ID=$(id -g)
-HOST_NAME=$(hostname 2>/dev/null || hostnamectl hostname)
-USER_ID=$(id -u)
-TZ=$(timedatectl | awk '/Time zone:/ {print $3}')
+ARCHITECTURE="$(uname -m)"
+GROUP_ID="$(id -g)"
+HOST_NAME="$(hostname 2>/dev/null || hostnamectl hostname)"
+USER_ID="$(id -u)"
+TZ="$(timedatectl | awk '/Time zone:/ {print $3}')"
 EOF
