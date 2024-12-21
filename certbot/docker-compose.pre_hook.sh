5d34fe1c336777379a6bb967c704126fd5dbc2f6
Saswat Padhi <saswatpadhi@protonmail.com>
2024-12-20 20:20:52 -0800

    minor

--- certbot/docker-compose.pre_hook.sh
+++ certbot/docker-compose.pre_hook.sh
@@ -5,4 +5,5 @@ set -Eumo pipefail
 SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 DATA_DIR="$SELF_DIR/data"
 
-mkdir -p "$DATA_DIR/certbot/etc/letsencrypt"
+mkdir -p "$DATA_DIR/certbot/etc/letsencrypt" \
+         "$DATA_DIR/certbot/var/log/letsencrypt"
