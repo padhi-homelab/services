e74a68842fb27e29236e3bee036d6e12957ad8b1
Saswat Padhi <saswatpadhi@protonmail.com>
2025-01-19 11:35:33 -0800

    netbox: redis -> valkey

--- netbox/docker-compose.pre_hook.sh
+++ netbox/docker-compose.pre_hook.sh
@@ -6,7 +6,7 @@ SELF_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 DATA_DIR="$SELF_DIR/data"
 
 mkdir -p "$DATA_DIR/postgres/var/lib/postgresql/data" \
-         "$DATA_DIR/redis/data" \
+         "$DATA_DIR/valkey/data" \
          "$DATA_DIR/netbox/opt/netbox/netbox/media" \
          "$DATA_DIR/netbox/opt/netbox/netbox/reports" \
          "$DATA_DIR/netbox/opt/netbox/netbox/scripts"
